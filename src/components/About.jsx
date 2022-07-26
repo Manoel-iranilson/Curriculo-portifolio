fa78f01f28646ae350438cb4b1590c8c160a6d00
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-07-25 23:20:40 -0300

    About

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function About() {
+  return (
+    <section className='about'>
+        <h2>Sobre</h2>
+        <p>Estou Fazendo faculdade de Sistemas da informação 
+            e procurar um estágio em desenvolvedor, já tenho conhecimento 
+            em programação web e mobile, e estou buscando cada vez mais conhecido 
+            para me torna um profissional excepcional !!</p>
+    </section>
+  );
+}
+
+export default About;
