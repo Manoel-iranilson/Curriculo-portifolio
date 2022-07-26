171f0cbd4276f898773c1969679f46164bcf9cd0
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-07-25 23:21:14 -0300

    ContentRepositories

---
+++ src/components/Repositories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Repositories() {
+  return (  
+    <section className="Repositories">
+    <h2>Projetos</h2>
+    <p>
+        Projetos que crie e estão disponivel no meu GitHub
+    </p>
+    <a href="https://github.com/Manoel-iranilson?tab=repositories" className="btn">
+        Ver Projetos
+    </a>
+    </section>
+    );
+}
+
+export default Repositories;
